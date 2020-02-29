fee210519c94f33181f83ad45d15165cb08bf7c7
ben <morantbenjamin@gmail.com>
2020-02-29 12:57:44 +0100

    avancée dans le tuto, j'essaie de me démerder tout seul

--- src/jeuAventureSwing/JeuSwing1.java
+++ src/jeuAventureSwing/JeuSwing1.java
@@ -11,11 +11,11 @@ public class JeuSwing1 {
     Container container;
     JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel, playerPanel;
     JLabel titleNameLabel, hpLabel, hpLabelNumber, weaponLabel, weaponLabelName;
-    Font titleFont = new Font("Times New Roman", Font.PLAIN, 42);
-    Font normalFont = new Font("Times New Roman", Font.PLAIN, 26);
+    Font titleFont = new Font("Noto Sans", Font.BOLD, 42);
+    Font normalFont = new Font("Noto Sans", Font.PLAIN, 26);
     JButton startButton, choice1, choice2, choice3, choice4;
     JTextArea mainTextArea;
-    int playerHp;
+    int playerHp, monsterHp, silverRing;
     String playerWeapon, position;
 
     TitleScreenHandler tsHandler = new TitleScreenHandler();
@@ -31,14 +31,14 @@ public class JeuSwing1 {
         window = new JFrame();
         window.setSize(800, 600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.getContentPane().setBackground(Color.darkGray);
+        window.getContentPane().setBackground(Color.black);
         window.setLayout(null);
         window.setVisible(true);
         container = window.getContentPane();
 
         titleNamePanel = new JPanel();
         titleNamePanel.setBounds(100, 100, 600, 150);
-        titleNamePanel.setBackground(Color.gray);
+        titleNamePanel.setBackground(Color.black);
         titleNameLabel = new JLabel("L'AVENTURE N'ATTEND PAS");
         titleNameLabel.setForeground(Color.white);
         titleNameLabel.setFont(titleFont);
@@ -48,8 +48,8 @@ public class JeuSwing1 {
         startButtonPanel.setBackground(Color.black);
 
         startButton = new JButton("C'EST PARTI !");
-        startButton.setBackground(Color.lightGray);
-        startButton.setForeground(Color.black);
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.white);
         startButton.setFont(normalFont);
         startButton.addActionListener(tsHandler);
         startButton.setFocusPainted(false);
@@ -68,7 +68,7 @@ public class JeuSwing1 {
 
         mainTextPanel = new JPanel();
         mainTextPanel.setBounds(100, 100, 600, 250);
-        mainTextPanel.setBackground(Color.darkGray);
+        mainTextPanel.setBackground(Color.black);
         container.add(mainTextPanel);
 
         mainTextArea = new JTextArea("Yo");
@@ -80,7 +80,7 @@ public class JeuSwing1 {
         mainTextPanel.add(mainTextArea);
 
         choiceButtonPanel = new JPanel();
-        choiceButtonPanel.setBounds(250, 350, 350, 150);
+        choiceButtonPanel.setBounds(250, 350, 300, 150);
         choiceButtonPanel.setBackground(Color.black);
         choiceButtonPanel.setLayout(new GridLayout(4, 1));
         container.add(choiceButtonPanel);
@@ -146,6 +146,7 @@ public class JeuSwing1 {
 
     public void playerSetup() {
         playerHp = 15;
+        monsterHp = 20;
         playerWeapon = "Couteau";
         weaponLabelName.setText(playerWeapon);
         hpLabelNumber.setText(""+playerHp);
@@ -155,20 +156,41 @@ public class JeuSwing1 {
 
     public void townGate() {
         position = "townGate";
-        mainTextArea.setText("Vous êtes à la porte de la Cité. Un garde se tient debout devant vous. \n Qu'est-ce que vous voulez faire ? \n \n \n ");
+        mainTextArea.setText("Vous êtes à la porte de la Cité. \n Un garde se tient debout devant vous. \n Que voulez vous faire ? \n \n \n ");
         choice1.setText("Parler au garde");
         choice2.setText("Attaquer le garde");
         choice3.setText("Partir de là");
-        choice3.setText("");
+        choice4.setText("");
     }
 
     public void talkGuard() {
         position = "talkGuard";
-        mainTextArea.setText("Garde: \" Bien le bonjour, étranger !\n Je ne vous ai jamais vu. Désolé mais nous ne pouvons pas laisser un étranger dans notre ville \"");
+        mainTextArea.setText("Garde: \" Bien le bonjour, étranger !\n Je ne vous ai jamais vu. \n Je suis désolé mais nous ne pouvons pas laisser  \n un étranger dans notre ville \"");
         choice1.setText(">");
         choice2.setText("");
         choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void attackGuard() {
+        position = "attackGuard";
+        mainTextArea.setText("Garde: \"Hey ! Ne soyez pas stupide!\" \n Vous vous battez bravement mais le garde vous frappe fort \net vous recevez trois points de dommage");
+        playerHp = playerHp - 2;
+        hpLabelNumber.setText("" + playerHp);
+        choice1.setText(">");
+        choice2.setText("");
         choice3.setText("");
+        choice4.setText("");
+    }
+
+    public void crossRoad() {
+        position = "crossRoad";
+        mainTextArea.setText("Vous êtes à un carrefour. \nSi vous allez au sud, vous serez de retour aux portes de la Cité. \n  Vous choisissez d'aller :");
+        choice1.setText("au Nord");
+        choice2.setText("à l'Est");
+        choice3.setText("au Sud");
+        choice4.setText("à l'Ouest");
+
     }
 
     public class TitleScreenHandler implements ActionListener {
@@ -194,6 +216,19 @@ public class JeuSwing1 {
                             talkGuard();
                             break;
                         case "c2":
+                            attackGuard();
+                            break;
+                        case "c3":
+                            crossRoad();
+                            break;
+                    }
+                case "talkGuard":
+                    switch (yourChoice) {
+                        case "c1":
+                            townGate();
+                            break;
+                        case "c2":
+                            attackGuard();
                             break;
                         case "c3":
                             break;
