db52d0b0d44eecea115df5bd20d4baf5d30cd3d3
bmorant <b.morant@lefebvre-sarrut.eu>
2020-03-02 15:35:58 +0100

    avancees

---
+++ src/JeuAventureSwingClassesMultiples/Game.java
@@ -0,0 +1,18 @@
+package JeuAventureSwingClassesMultiples;
+
+public class Game {
+
+    UI ui = new UI();
+
+    public Game() {
+
+        ui.createUI();
+
+    }
+
+    public static void main(String[] args) {
+
+        new Game();
+
+    }
+}
