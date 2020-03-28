c4da0addb8e6c0a79cc738e7bf56472944164cf2
bmorant <b.morant@lefebvre-sarrut.eu>
2020-03-28 18:28:11 +0100

    seeing what might be coming from the tutorial

--- src/main/java/adventuregame/model/uielements/Board.java
+++ src/main/java/adventuregame/model/uielements/Board.java
@@ -3,6 +3,8 @@ package adventuregame.model.uielements;
 import adventuregame.model.items.weapons.Knife;
 import adventuregame.model.people.Hero;
 
+import java.util.Arrays;
+
 public class Board {
 
     private char[][] boardData = new char[20][20];
@@ -13,12 +15,24 @@ public class Board {
         hero = new Hero(2, "Loup Ardent", "rambo.jpg", "Seriez vous l'élu ?", new Knife(), null);
     }
 
-    @Override
+
     public String toString() {
-//        return "Board{" +
-//                "boardData=" + Arrays.toString(boardData) +
-//                ", hero=" + hero +
-//                '}';
-        return boardData.toString();
+        return "Board{" +
+                "boardData=" + Arrays.toString(boardData) +
+                ", hero=" + hero +
+                '}';
+
+//        return boardData.toString();
+    }
+
+    public char[][] getBoardDatar() {
+        for (int i = 0; i < boardData.length; i++) {
+            for (int o = 0; o < boardData[i].length; o++) {
+                boardData[i][o] = 'x';
+                //     System.out.print(boardData[i][o]);
+            }
+            //   System.out.println( );
+        }
+        return boardData;
     }
 }
