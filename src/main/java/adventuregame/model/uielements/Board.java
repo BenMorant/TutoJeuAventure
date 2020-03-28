92f4b5e6531544a00a444ca634f6ba1b371dc8be
bmorant <b.morant@lefebvre-sarrut.eu>
2020-03-28 15:52:05 +0100

    doing this tutorial : https://www.youtube.com/watch?v=LH7ZwSLtnCM
    now litteraly, then I will adapat it to the game I wanna make (I think I know how...)

--- src/main/java/adventuregame/model/uielements/Board.java
+++ src/main/java/adventuregame/model/uielements/Board.java
@@ -3,23 +3,22 @@ package adventuregame.model.uielements;
 import adventuregame.model.items.weapons.Knife;
 import adventuregame.model.people.Hero;
 
-import java.util.Arrays;
-
 public class Board {
 
-    private char[][] boardData;
+    private char[][] boardData = new char[20][20];
     private Hero hero;
 
     public Board() {
-        boardData = null;
+
         hero = new Hero(2, "Loup Ardent", "rambo.jpg", "Seriez vous l'élu ?", new Knife(), null);
     }
 
     @Override
     public String toString() {
-        return "Board{" +
-                "boardData=" + Arrays.toString(boardData) +
-                ", hero=" + hero +
-                '}';
+//        return "Board{" +
+//                "boardData=" + Arrays.toString(boardData) +
+//                ", hero=" + hero +
+//                '}';
+        return boardData.toString();
     }
 }
